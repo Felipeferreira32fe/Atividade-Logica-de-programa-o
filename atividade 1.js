7268fdbd2f9585986a796c9c296b5bfe5310e9c9
Felipe_Ferreira@ <152656879+Felipeferreira32fe@users.noreply.github.com>
2023-12-19 18:07:28 -0300

    Add files via upload

---
+++ atividade 1.js
@@ -0,0 +1,25 @@
+alert('Boas vindas');
+
+let nome = luar;
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivél = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemDeErro = 'Erro! preencha todos os campos';
+alert('mensagemDeErro');
+
+let nome = prompt('Digite seu nome');
+let nome = prompt('Digite sua idade');
+
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
+
+
+
+
+
+
